50f637706f3ece19bc2360845fbed123af5d0763
Tech-A35 <Tech-A35@Tech-A35s-MacBook-Air.local>
2017-10-31 12:08:11 +0000

    initial commit

---
+++ function-starter-code/js/index.js
@@ -0,0 +1,28 @@
+
+
+function addTwo(n1 , n2) {
+}
+
+function addThree(n1, n2, n3) {
+}
+
+function multiplyTwo(n1,n2) {
+}
+
+function increaseByPercentage(total, percentage) {
+}
+
+function reverseWord(word) {
+}
+
+function celciusToFahrenheit(celcius) {
+}
+
+function fahrenheitToCelcius(fahr) {
+}
+
+// BONUS QUESTION
+function stripVowels(word) {
+}
+
+runTests();
